6b7998be64fa53534081647c8b9111682a6c7c96
Deddel <83078233+deddel@users.noreply.github.com>
2024-09-19 16:58:05 +0200

    Add project files.

---
+++ Ovning3/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    internal class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private int height;
+        private int weight;
+
+        public int Age 
+        { 
+            get { return age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    string msg = "Age must be larger than 0!";
+                    throw new ArgumentException(msg);
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string FName { get { return fName; } set { fName = value; } }
+        public string LName { get {return lName; } set { lName = value; } }
+        public int Height { get { return height; } set { height = value; } }
+        public int Weight { get { return weight; } set { weight = value; } }
+
+    }
+}
---
+++ Ovning3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ovning3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            
+            while (isRunning) 
+            {
+                try
+                {
+                    Person p = new Person();
+                    p.LName = "Lukas";
+                    p.FName = "Alexander"; //Ja man kommer åt properties men inte de privata fälten
+                    p.Age = -1;
+                    p.Height = 176;
+                    p.Weight = 70;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    isRunning = false;
+                }
+                finally
+                {
+                    if (isRunning == false)
+                        Console.WriteLine("There was an exception error. Stopping program");
+                    else 
+                        Console.WriteLine("No exeption errors");
+                }
+                
+            }
+        }
+    }
+}
