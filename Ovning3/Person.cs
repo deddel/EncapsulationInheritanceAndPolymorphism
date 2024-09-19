5b1aef20dc3bb3ea0393881b03f994461d6d561b
Deddel <83078233+deddel@users.noreply.github.com>
2024-09-19 18:16:48 +0200

    3.1.1

--- Ovning3/Person.cs
+++ Ovning3/Person.cs
@@ -15,6 +15,15 @@ namespace Ovning3
         private int height;
         private int weight;
 
+        public Person(int age, string fName, string lName, int height, int weight)
+        {
+            Age = age;
+            FName = fName;
+            LName = lName;
+            Height = height;
+            Weight = weight;
+        }
+
         public int Age 
         { 
             get { return age; }
@@ -22,8 +31,8 @@ namespace Ovning3
             {
                 if (value <= 0)
                 {
-                    string msg = "Age must be larger than 0!";
-                    throw new ArgumentException(msg);
+                    string exMsg = "Age must be larger than 0 !!";
+                    throw new ArgumentException(exMsg);
                 }
                 else
                 {
@@ -31,8 +40,46 @@ namespace Ovning3
                 }
             }
         }
-        public string FName { get { return fName; } set { fName = value; } }
-        public string LName { get {return lName; } set { lName = value; } }
+        public string FName
+        { 
+            get { return fName; } 
+            set 
+            {
+                //if (value == null || value =="")
+                if (string.IsNullOrEmpty(value))
+                {
+                    string exMsg = "FName can not be null or empty!!";
+                    throw new ArgumentException(exMsg);
+                }
+                else if (value.Length < 2 || value.Length > 10)
+                {
+                    string exMsg = "FName must be between 2 and 10 charachters !!";
+                    throw new ArgumentException(exMsg);
+                }
+                else
+                    fName = value;
+            } 
+        }
+        public string LName
+        { 
+            get { return lName; } 
+            set 
+            {
+                //if (value == null || value =="")
+                if (string.IsNullOrEmpty(value))
+                {
+                    string exMsg = "LName can not be null or empty!!";
+                    throw new ArgumentException(exMsg);
+                }
+                else if (value.Length < 3 || value.Length > 15)
+                {
+                    string exMsg = "LName must be between 3 and 15 charachters !!";
+                    throw new ArgumentException(exMsg);
+                }
+                else
+                    lName = value;
+            } 
+        }
         public int Height { get { return height; } set { height = value; } }
         public int Weight { get { return weight; } set { weight = value; } }
 
--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -10,12 +10,25 @@
             {
                 try
                 {
-                    Person p = new Person();
-                    p.LName = "Lukas";
-                    p.FName = "Alexander"; //Ja man kommer åt properties men inte de privata fälten
-                    p.Age = -1;
-                    p.Height = 176;
-                    p.Weight = 70;
+                    //Test data
+                    Person p1 = new Person(8, "Alexander", "Lukas", 176, 70);
+                    Person p2 = new Person(15, "Farmor", "Anka", 165, 63);
+                    
+                    //Uncomment one of theese at a time
+                    //Person p3 = new Person(9, null, "Anka", 165, 63);
+                    //Person p3 = new Person(9, "", "Anka", 165, 63);
+                    //Person p3 = new Person(9, "K", "Anka", 165, 63);
+                    //Person p3 = new Person(9, "Kalle", "A", 165, 63);
+                    //Person p3 = new Person(9, "Kalle", "", 165, 63);
+                    //Person p3 = new Person(9, "Kalle", null, 165, 63);
+                    
+                    //Checking access
+                    p1.FName = "Farbror"; //Ja man kommer åt properties men inte de privata fälten
+                    p1.LName = "Joakim";
+                    p1.Age = 12;
+                    p1.Height = 168;
+                    p1.Weight = 69;
+                    //p1.age = 12; //Error: Person.age is inaccessible due to its protection level         
                 }
                 catch (ArgumentException ex)
                 {
@@ -28,6 +41,7 @@
                         Console.WriteLine("There was an exception error. Stopping program");
                     else 
                         Console.WriteLine("No exeption errors");
+                        isRunning = false;
                 }
                 
             }
